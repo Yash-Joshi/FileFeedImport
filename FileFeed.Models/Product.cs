9ae55ee65f672a5d3cb5af91113fa6d2960f2787
Yash Joshi <iyashjoshi@gmail.com>
2021-02-01 20:55:17 +0530

    changes

---
+++ FileFeed.Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileFeeds.Models
+{
+    /// <summary>
+    /// This is a root class that will store the array of products that is been passed to the solution
+    /// </summary>
+    public class Rootobject
+    {
+        public Product[] products { get; set; }
+    }
+
+    /// <summary>
+    /// This is the base class for which the object will be saved and dispalyed to the console application
+    /// This can be further utilized to save data to database   
+    /// </summary>
+    public class Product
+    {
+        public string[] categories { get; set; }
+        public string twitter { get; set; }
+        public string title { get; set; }
+    }
+}
---
+++ FileFeed.Models/YamlProducts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileFeeds.Models
+{
+    public class RootProducts
+    {
+        public YamlProducts[] yamlProducts { get; set; }
+    }
+    public class YamlProducts
+    {
+        public string tag { get; set; }
+
+        public string name { get; set; }
+
+        public string twitter { get; set; }
+
+    }
+}
